f2ec606352c21f62b27e01f8bf9ee882e14b331a
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-12-29 06:39:34 -0500

    test api created

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4hum0hz.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+       
+        const tasksCollection = client.db('TaskHandler').collection('tasks');
+
+        app.post('/tasks', async (req, res) => {
+            const task = req.body;
+            console.log(task);
+            const result = await tasksCollection.insertOne(task);
+            res.send(result)
+        })
+
+
+        
+        app.get('/mytasks/:email', async (req, res) => {
+            const email=req.params.email;
+            const query = {userEmail:email,completeStatus:'Not Completed'}
+            const result = await tasksCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.get('/completedtasks/:email', async (req, res) => {
+            const email=req.params.email;
+            const query = {userEmail:email,completeStatus:'Completed'}
+            const result = await tasksCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await tasksCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+        app.get()
+
+        app.put('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
+   
+
+        
+
+    }
+    finally {
+
+    }
+
+
+}
+run().catch(err => console.log(err))
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('This is home page')
+})
+
+app.listen(port, () => {
+    console.log(`app is listening ${port}`)
+})
